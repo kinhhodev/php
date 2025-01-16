db2f5720088345f8a20ad57831293fc886fcfc76
Kinh Ho <hongockinh@outlook.com>
2025-01-16 11:29:08 +0700

    Test Indent 1

---
+++ test-indent.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="img w-bg">
+    <picture class="image--mobile">
+        <source media="(min-width: 768px)" srcset="">
+        <img class="object-cover" srcset="" loading="eager" alt="herobanner" fetchpriority="true">
+    </picture>
+
+    <picture class="image--ipad-up">
+        <source media="(max-width: 767px)" srcset="">
+        <img class="object-cover" srcset="" loading="eager" alt="herobanner" fetchpriority="true">
+    </picture>
+</div>
+</body>
+</html>
