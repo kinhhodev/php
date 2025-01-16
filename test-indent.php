cf0f9c619861b5af8a5721c40f0e266e49a77740
Kinh Ho <hongockinh@outlook.com>
2025-01-16 11:30:34 +0700

    Test Indent 2

--- test-indent.php
+++ test-indent.php
@@ -10,12 +10,25 @@
     <picture class="image--mobile">
         <source media="(min-width: 768px)" srcset="">
         <img class="object-cover" srcset="" loading="eager" alt="herobanner" fetchpriority="true">
+
     </picture>
 
     <picture class="image--ipad-up">
         <source media="(max-width: 767px)" srcset="">
         <img class="object-cover" srcset="" loading="eager" alt="herobanner" fetchpriority="true">
+        <div class="img w-bg">
+            <picture class="image--mobile">
+                <source media="(min-width: 768px)" srcset="<?php echo get_template_directory_uri(); ?>/assets/images/empty.jpg">
+                <img class="object-cover" srcset="<?php echo get_template_directory_uri(); ?>/assets/images/contact_bg_mobile.png" loading="eager" alt="herobanner" fetchpriority="true">
+            </picture>
+
+            <picture class="image--ipad-up">
+                <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/assets/images/empty.jpg">
+                <img class="object-cover" srcset="<?php echo get_template_directory_uri(); ?>/assets/images/contact_bg.png" loading="eager" alt="herobanner" fetchpriority="true">
+            </picture>
+        </div>
     </picture>
+    
 </div>
 </body>
 </html>
